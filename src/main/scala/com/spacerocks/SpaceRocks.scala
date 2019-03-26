fbd1efd9eb7b6c8c6d197a919567b84b3be48051
Doug Beasley <beasley.doug@gmail.com>
2019-03-26 14:31:21 -0700

    setting config bootstrap

--- src/main/scala/com/spacerocks/SpaceRocks.scala
+++ src/main/scala/com/spacerocks/SpaceRocks.scala
@@ -1,39 +1,69 @@
 package com.spacerocks
 
 import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.cluster.Cluster
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.Message
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
+import akka.management.cluster.bootstrap.ClusterBootstrap
+import akka.management.scaladsl.AkkaManagement
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
 import akka.util.Timeout
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import com.spacerocks.RockControlActor.GetControlFlow
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import akka.pattern.ask
 import com.spacerocks.RockListenerActor.GetListenerFlow
 
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
-object SpaceRocks extends App {
+
+object Rock1 extends App {
+  new SpaceRocks(1)
+}
+
+object Rock2 extends App {
+  new SpaceRocks(2)
+}
+
+object Rock3 extends App {
+  new SpaceRocks(3)
+}
+
+class SpaceRocks(nr : Int) {
 
   implicit val askTimeout = Timeout(5.seconds)
 
-  val config = ConfigFactory.load()
-  val clusterName = config.getString("clustering.cluster.name")
+//  val config = ConfigFactory.load()
+//  val clusterName = config.getString("clustering.cluster.name")
   val SpaceRockTopic = "space-rocks"
 
-  implicit val system = ActorSystem(clusterName)
+  val config: Config = ConfigFactory.parseString(s"""
+      akka.remote.artery.canonical.hostname = "127.0.0.$nr"
+      akka.management.http.hostname = "127.0.0.$nr"
+    """).withFallback(ConfigFactory.load())
+  val system = ActorSystem("space-rocks", config)
+
+  AkkaManagement(system).start()
+
+  ClusterBootstrap(system).start()
+
+  Cluster(system).registerOnMemberUp({
+    system.log.info("Cluster is up!")
+  })
+
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
   val clusterListener = system.actorOf(Props[ClusterListener], name = "clusterListener")
   val subscriber = system.actorOf(RockListenerActor.props(SpaceRockTopic))
 
-  def actorFlow(ref : ActorRef): Flow[Message, Message, Any] = Flow[Message].ask[Message](ref)
 
+  //TODO Move http stuff to a different file
+  def actorFlow(ref : ActorRef): Flow[Message, Message, Any] = Flow[Message].ask[Message](ref)
 
   val route =
     path("control") {
@@ -63,13 +93,5 @@ object SpaceRocks extends App {
     }
 
   val bindingFuture = Http().bindAndHandle(cors() {route}, "0.0.0.0", 8080)
-
-  /**
-      println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-      StdIn.readLine() // let it run until user presses return
-      bindingFuture
-        .flatMap(_.unbind()) // trigger unbinding from the port
-        .onComplete(_ => system.terminate()) // and shutdown when done
-    */
 
 }
