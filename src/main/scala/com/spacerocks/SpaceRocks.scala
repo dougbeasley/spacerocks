48d2704ea845e23ba8a03a4cf2f1d879268ba15a
Doug Beasley <beasley.doug@gmail.com>
2019-04-18 16:52:51 -0700

    code cleanup

--- src/main/scala/com/spacerocks/SpaceRocks.scala
+++ src/main/scala/com/spacerocks/SpaceRocks.scala
@@ -1,16 +1,12 @@
 package com.spacerocks
 
 import akka.actor.{ActorSystem, Props}
-import akka.cluster.{Cluster, MemberStatus}
+import akka.cluster.Cluster
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes
 import akka.management.cluster.bootstrap.ClusterBootstrap
 import akka.management.scaladsl.AkkaManagement
 import akka.stream.ActorMaterializer
-import akka.util.Timeout
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
-
-import scala.concurrent.duration._
 
 
 object SpaceRocks extends App with RockRouting {
@@ -30,32 +26,11 @@ object SpaceRocks extends App with RockRouting {
   AkkaManagement(system).start()
   ClusterBootstrap(system).start()
 
-  val clusterListener = system.actorOf(Props[ClusterListener], name = "clusterListener")
-  val subscriber = system.actorOf(RockListenerActor.props(SpaceRockTopic))
-
-  implicit val askTimeout = Timeout(5.seconds)
-
-
-  val healthRoutes = path("alive") {
-      complete(StatusCodes.OK)
-    } ~
-    path("ready") {
-      val selfState = cluster.selfMember.status
-      log.debug(s"ready? clusterState: $selfState")
-
-      selfState match {
-        case MemberStatus.Up => complete(StatusCodes.OK)
-        case MemberStatus.WeaklyUp => complete(StatusCodes.OK)
-        case _ => complete(StatusCodes.InternalServerError)
-      }
-    }
-
-
-
-  val bindingFuture = Http().bindAndHandle(cors() { routes } ~ healthRoutes, "0.0.0.0", 8080)
+  val bindingFuture = Http().bindAndHandle(cors() { routes }, "0.0.0.0", 8080)
 
   Cluster(system).registerOnMemberUp({
     log.info("Cluster member is up!")
+    system.actorOf(Props[ClusterListener], name = "clusterListener")
   })
 
 }
